70a6852d457ff70565dbfbbc2cc36c8f82128b81
amubb <15051833621@163.com>
2019-08-27 17:23:03 +0800

    format backend/controllers/PageController.php

--- backend/controllers/PageController.php
+++ backend/controllers/PageController.php
@@ -2,21 +2,20 @@
 
 namespace backend\controllers;
 
+use backend\components\BaseController;
 use common\models\Page;
 use Yii;
-use backend\components\BaseController;
-use yii\web\NotFoundHttpException;
 use yii\data\ActiveDataProvider;
+use yii\web\NotFoundHttpException;
 
 /**
- * PageController implements the CRUD actions for Content model.
+ * Class PageController
+ * @package backend\controllers
  */
 class PageController extends BaseController
 {
-
     /**
-     * Lists all Content models.
-     * @return mixed
+     * @return string
      */
     public function actionIndex()
     {
@@ -28,23 +27,19 @@ class PageController extends BaseController
         ]);
     }
 
-
     /**
-     * Creates a new Content model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
+     * @return string|\yii\web\Response
      */
     public function actionCreate()
     {
         $model = new Page();
-        $model->allowComment=true;
-        $model->allowFeed=true;
-        $model->allowPing=true;
-        if(Yii::$app->request->isPost){
-            if($model->load(Yii::$app->request->post())){
-                $model->inputAttachments=Yii::$app->request->post('inputAttachments',[]);
+        $model->allowComment = true;
+        $model->allowFeed = true;
+        $model->allowPing = true;
+        if (Yii::$app->request->isPost) {
+            if ($model->load(Yii::$app->request->post())) {
+                $model->inputAttachments = Yii::$app->request->post('inputAttachments', []);
                 if ($model->save()) {
-
                     return $this->redirect(['index']);
                 }
             }
@@ -55,25 +50,20 @@ class PageController extends BaseController
     }
 
     /**
-     * Updates an existing Content model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
+     * @param $id
+     * @return string|\yii\web\Response
      */
     public function actionUpdate($id)
     {
         $model = $this->findModel($id);
 
-        if(Yii::$app->request->isPost){
-
-            if($model->load(Yii::$app->request->post())){
-                $model->inputAttachments=Yii::$app->request->post('inputAttachments',[]);
+        if (Yii::$app->request->isPost) {
+            if ($model->load(Yii::$app->request->post())) {
+                $model->inputAttachments = Yii::$app->request->post('inputAttachments', []);
                 if ($model->save()) {
-
                     return $this->redirect(['index']);
                 }
             }
-
         }
 
         return $this->render('update', [
@@ -82,28 +72,23 @@ class PageController extends BaseController
     }
 
     /**
-     * Deletes an existing Content model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
+     * @param $id
+     * @return \yii\web\Response
      */
     public function actionDelete($id)
     {
-
         $this->findModel($id)->delete();
         return $this->redirect(['index']);
     }
 
     /**
-     * Finds the Content model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return Page the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
+     * @param $id
+     * @return array|null|\yii\db\ActiveRecord
+     * @throws NotFoundHttpException
      */
     protected function findModel($id)
     {
-        if (($model = Page::find()->andWhere(['cid'=>$id])->one()) !== null) {
+        if (($model = Page::find()->andWhere(['cid' => $id])->one()) !== null) {
             return $model;
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');
