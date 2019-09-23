ee19881e21b486f0d94887418b041641fcb41b7a
amubb <15051833621@163.com>
2019-09-23 15:40:28 +0800

    右侧展示分类+文章目录

--- common/widgets/CategoryList.php
+++ common/widgets/CategoryList.php
@@ -3,6 +3,7 @@
 namespace common\widgets;
 
 use common\components\CategoryTree;
+use common\models\Category;
 use yii;
 use yii\helpers\Html;
 
@@ -38,9 +39,26 @@ class CategoryList extends yii\base\Widget
                 $this->_htmlStr .= '<li>';
                 $this->_htmlStr .= str_repeat('&nbsp;&nbsp;&nbsp;&nbsp;', $v['depth'] - 1) . Html::a($v['name'] . '(' . $v['count'] . ')', ['site/category', 'slug' => $v['slug']], $this->options);
                 $this->_htmlStr .= '</li>';
+
+                $category= Category::find()->andWhere(['slug'=>$v['slug']])->one();
+                $posts=$category->getPosts()->all();
+                foreach ($posts as $post) {
+                    $this->_htmlStr .= '<li class="li-overflow-hidden">';
+                    $this->_htmlStr .= str_repeat(
+                        '&nbsp;&nbsp;&nbsp;&nbsp;', $v['depth']) . Html::a($post['title'],
+                            ['post/'.$post['cid']],
+                            // $this->options
+                            [
+                                'style' => [
+                                    'color' => 'black',
+                                    'font-size' => '14px',
+                                ],
+                            ]
+                        );
+                    $this->_htmlStr .= '</li>';
+                }
             }
         }
-
         $this->_htmlStr .= '</ul>';
     }
 
